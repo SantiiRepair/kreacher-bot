324e52ff3fbf07161e59d15e19c543822fdeafc2
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-12 08:58:46 +0000

    Update cron.py

--- bot/tasks/cron.py
+++ bot/tasks/cron.py
@@ -1,9 +1,8 @@
 import os
-from getpass import getuser
 from crontab import CronTab
 
 
-cron = CronTab(user=getuser())
+cron = CronTab(user=True)
 current_dir = os.path.dirname(os.path.abspath(__file__))
 file = os.path.join(current_dir, "dev.py")
 job = cron.new(command=f"python3 {file}")
