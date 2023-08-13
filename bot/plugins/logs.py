fd3a10f72136490eb2b383b4e6a45912f0a68078
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-13 18:30:11 +0000

    Update logs.py

--- bot/plugins/logs.py
+++ bot/plugins/logs.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from bot import kreacher, config
 from pyrogram import filters, Client
 from pyrogram.types import Message
@@ -6,11 +7,17 @@ from pyrogram.types import Message
 
 @kreacher.on_message(filters.regex(pattern="^[!?/]logs"))
 async def _(client: Client, message: Message):
-    dev = await client.get_users(config.MANTAINER)
-    if not dev.id == message.from_user.id:
-        return await message.reply(
-            f"**__Only [{dev.first_name}](https://t.me/{dev.username}), can execute this command__** \U0001f6ab"
+    try:
+        dev = await client.get_users(config.MANTAINER)
+        if not dev.id == message.from_user.id:
+            return await message.reply(
+                f"**__Only [{dev.first_name}](https://t.me/{dev.username}), can execute this command__** \U0001f6ab"
+            )
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        document = os.path.join(current_dir, "../logs/logs.txt")
+        await message.reply_document(document=document)
+    except Exception as e:
+        logging.error(e)
+        await message.reply(
+            f"**__Oops master, something wrong has happened.__** \n\n`Error: {e}`",
         )
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    document = os.path.join(current_dir, "../logs/logs.txt")
-    await message.reply_document(document=document)
