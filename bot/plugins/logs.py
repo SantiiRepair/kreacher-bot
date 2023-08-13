ab4163133fdae4a34bf26b7ea3edde6ebbf87c6d
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-13 18:41:45 +0000

    Update logs.py

--- bot/plugins/logs.py
+++ bot/plugins/logs.py
@@ -11,7 +11,8 @@ async def _(client: Client, message: Message):
         dev = await client.get_users(config.MANTAINER)
         if not dev.id == message.from_user.id:
             return await message.reply(
-                f"**__Only [{dev.first_name}](https://t.me/{dev.username}), can execute this command__** \U0001f6ab"
+                f"**__Only [{dev.first_name}](https://t.me/{dev.username}), can execute this command__** \U0001f6ab",
+                disable_web_page_preview=True,
             )
         current_dir = os.path.dirname(os.path.abspath(__file__))
         document = os.path.join(current_dir, "../logs/logs.txt")
