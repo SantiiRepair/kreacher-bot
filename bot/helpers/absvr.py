4e21d7e179ec2415a959c5bd525d31c677c41fdb
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-20 17:46:44 +0000

    Update absvr.py

--- bot/helpers/absvr.py
+++ bot/helpers/absvr.py
@@ -2,9 +2,13 @@ import os
 import pyttsx3
 
 
-async def absvr(text, path):
+async def absvr(text, path, rate=125, volume=1.0, i=1):
     if not os.path.exists(os.path.dirname(path)):
         os.makedirs(os.path.dirname(path))
     engine = pyttsx3.init()
+    engine.setProperty("rate", rate)
+    engine.setProperty("volume", volume)
+    voices = engine.getProperty("voices")
+    engine.setProperty("voice", voices[i].id)
     engine.save_to_file(text, path)
     engine.runAndWait()
