46a2f4abcf0103789c478dc3b1aa48f1a6c3f6b0
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-20 19:42:48 +0000

    Update 3 files

--- bot/helpers/translator.py
+++ bot/helpers/translator.py
@@ -1,6 +1,11 @@
+import logging
 from translate import Translator
 
 
 async def translator(text: str, to_lang: str):
-    translator = Translator(to_lang=to_lang)
-    return translator.translate(text)
+    try:
+        translator = Translator(to_lang=to_lang)
+        return translator.translate(text)
+    except Exception as e:
+        logging.error(e)
+        raise e
--- bot/helpers/tts.py
+++ bot/helpers/tts.py
@@ -1,15 +1,20 @@
 import os
+import logging
 from gtts import gTTS
 from bot.helpers.translator import translator
 
 
 async def tts(text: str, path: str, to_lang="", lang="en", slow=False):
-    if not os.path.exists(os.path.dirname(path)):
-        os.makedirs(os.path.dirname(path))
-    if to_lang:
-        translation = await translator(text, to_lang)
-        gtts = gTTS(text=translation, lang=lang, slow=slow)
+    try:
+        if not os.path.exists(os.path.dirname(path)):
+            os.makedirs(os.path.dirname(path))
+        if to_lang:
+            translation = await translator(text, to_lang)
+            gtts = gTTS(text=translation, lang=lang, slow=slow)
+            gtts.save(path)
+            return
+        gtts = gTTS(text=text, lang=lang, slow=slow)
         gtts.save(path)
-        return
-    gtts = gTTS(text=text, lang=lang, slow=slow)
-    gtts.save(path)
+    except Exception as e:
+        logging.error(e)
+        raise e
