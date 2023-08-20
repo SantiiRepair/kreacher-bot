4d58a401e71b4219c81c9f6a3c3f147f1467c7ed
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-20 17:51:18 +0000

    Update vcs.py

--- bot/tasks/vcs.py
+++ bot/tasks/vcs.py
@@ -1,7 +1,7 @@
 import os
+import logging
 from bot import on_call
-from bot.helpers.handler import skip_current
-from bot.helpers.queues import clear_queue, get_active_chats
+
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 queues = os.path.join(current_dir, "../dbs/queues.pkl")
@@ -10,14 +10,17 @@ actives = os.path.join(current_dir, "../dbs/actives.pkl")
 
 @on_call.on_audio_playout_ended
 async def audio_ended(gc, source):
-    print(f"audio ended: {source}")
+    logging.info(f"audio ended: {source}")
+    os.remove(source)
 
 
 @on_call.on_video_playout_ended
 async def video_ended(gc, source):
-    print(f"video ended: {source}")
+    logging.info(f"video ended: {source}")
+    os.remove(source)
 
 
 @on_call.on_playout_ended
 async def media_ended(gc, source, media_type):
-    print(f"{media_type} ended: {source}")
+    logging.info(f"{media_type} ended: {source}")
+    os.remove(source)
