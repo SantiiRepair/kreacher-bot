07556e88d7473c7d0d4c1aab88e64a4c7105b2eb
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-20 22:07:35 +0000

    Update 6 files

--- bot/helpers/tts.py
+++ bot/helpers/tts.py
@@ -1,20 +1,42 @@
 import os
 import logging
-from gtts import gTTS
+from bot.helpers.bash import bash
 from bot.helpers.translator import translator
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+models = os.path.join(current_dir, "../models")
 
-async def tts(text: str, path: str, to_lang="", lang="en", slow=False):
+
+async def tts(text: str, path: str, to_lang="", lang="en_us", slow=False):
+    mdn = ""
+    if lang.lower() == "en_us":
+        mdn += "en_US-amy-medium"
+    elif lang.lower() == "en_gb":
+        mdn += "en_GB-alba-medium"
+    elif lang.lower() == "es_es":
+        mdn += "es_ES-davefx-medium"
+    elif lang.lower() == "es_mx":
+        mdn += "es_MX-ald-medium"
+    elif lang.lower() == "fr_fr":
+        mdn += "fr_FR-siwis-medium"
+    elif lang.lower() == "pt_br":
+        mdn += "pt_BR-faber-medium"
+    elif lang.lower() == "pt_pt":
+        mdn += "pt_PT-tugão-medium"
+    elif lang.lower() == "ru_ru":
+        mdn += "ru_RU-irina-medium"
     try:
         if not os.path.exists(os.path.dirname(path)):
             os.makedirs(os.path.dirname(path))
         if to_lang:
             translation = await translator(text, to_lang)
-            gtts = gTTS(text=translation, lang=lang, slow=slow)
-            gtts.save(path)
+            await bash(
+                f"echo '{translation}' | piper --model {mdn} --download-dir {models} --output_file {path}"
+            )
             return
-        gtts = gTTS(text=text, lang=lang, slow=slow)
-        gtts.save(path)
+        await bash(
+            f"echo '{text}' | piper --model {mdn} --download-dir {models} --output_file {path}"
+        )
     except Exception as e:
         logging.error(e)
         raise e
--- bot/helpers/yt.py
+++ bot/helpers/yt.py
@@ -1,23 +1,11 @@
 import os
-import asyncio
 import logging
 from yt_dlp import YoutubeDL
+from bot.helpers.bash import bash
 from youtubesearchpython import VideosSearch
 
 LOGS = {}
 SUDO_USERS = {}
-
-
-async def bash(cmd):
-    process = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    stdout, stderr = await process.communicate()
-    err = stderr.decode().strip()
-    out = stdout.decode().strip()
-    return out, err
 
 
 ydl = YoutubeDL(
