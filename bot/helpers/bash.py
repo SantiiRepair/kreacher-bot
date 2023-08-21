a97d1a8bd7cab305d0664b4ed6e9c335763ed138
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-21 06:42:48 +0000

    Update bash.py and tts.py

--- bot/helpers/bash.py
+++ bot/helpers/bash.py
@@ -1,13 +1,18 @@
 import asyncio
+import logging
 
 
 async def bash(cmd):
-    process = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    stdout, stderr = await process.communicate()
-    err = stderr.decode().strip()
-    out = stdout.decode().strip()
-    return out, err
+    try:
+        process = await asyncio.create_subprocess_shell(
+            cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await process.communicate()
+        err = stderr.decode().strip()
+        out = stdout.decode().strip()
+        return out, err
+    except Exception as e:
+        logging.error(e)
+        raise e
--- bot/helpers/tts.py
+++ bot/helpers/tts.py
@@ -1,44 +1,66 @@
 import os
 import logging
 from bot.helpers.bash import bash
-from bot.helpers.translator import translator
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 models = os.path.join(current_dir, "../models")
 
 
-async def tts(text: str, path: str, to_lang="", lang="en_us"):
-    mdn = ""
-    match lang.lower():
-        case "en_us":
-            mdn += "en_US-amy-medium"
-        case "en_gb":
-            mdn += "en_GB-alba-medium"
-        case "es_es":
-            mdn += "es_ES-davefx-medium"
-        case "es_mx":
-            mdn += "es_MX-ald-medium"
-        case "fr_fr":
-            mdn += "fr_FR-siwis-medium"
-        case "pt_br":
-            mdn += "pt_BR-faber-medium"
-        case "pt_pt":
-            mdn += "pt_PT-tugão-medium"
-        case "ru_ru":
-            mdn += "ru_RU-irina-medium"
+async def tts(text: str, path: str):
+    voice = _get_voice()
     try:
-        if not os.path.exists(os.path.dirname(path)):
+        if not os.path.exists(models):
+            os.makedirs(models)
+        elif not os.path.exists(os.path.dirname(path)):
             os.makedirs(os.path.dirname(path))
-        if to_lang:
-            translation = await translator(text, to_lang)
-            await bash(
-                f"echo '{translation}' | piper --download-dir {models} --model {mdn} --output_file {path}"
-            )
-            return
-        print(f"echo '{text}' | piper --download-dir {models} --model {mdn} --output_file {path}")
+        print(
+            f"echo '{text}' | piper -m {voice} --download-dir {models} --data-dir {models} -f {path}"
+        )
         await bash(
-            f"echo '{text}' | piper --download-dir {models} --model {mdn} --output_file {path}"
+            f"echo '{text}' | piper -m {voice} --download-dir {models} --data-dir {models} -f {path}"
         )
     except Exception as e:
         logging.error(e)
         raise e
+
+
+def _get_voice(lang="es_es", gender="female") -> str:
+    mdn = ""
+    if gender == "female":
+        match lang.lower():
+            case "en_us":
+                mdn += "en_US-amy-medium"
+            case "en_gb":
+                mdn += "en_GB-alba-medium"
+            case "es_es":
+                mdn += "es_ES-davefx-medium"
+            case "es_mx":
+                mdn += "es_MX-ald-medium"
+            case "fr_fr":
+                mdn += "fr_FR-siwis-medium"
+            case "pt_br":
+                mdn += "pt_BR-faber-medium"
+            case "pt_pt":
+                mdn += "pt_PT-tugão-medium"
+            case "ru_ru":
+                mdn += "ru_RU-irina-medium"
+        return mdn
+    elif gender == "male":
+        match lang.lower():
+            case "en_us":
+                mdn += "en_US-amy-medium"
+            case "en_gb":
+                mdn += "en_GB-alba-medium"
+            case "es_es":
+                mdn += "es_ES-davefx-medium"
+            case "es_mx":
+                mdn += "es_MX-ald-medium"
+            case "fr_fr":
+                mdn += "fr_FR-siwis-medium"
+            case "pt_br":
+                mdn += "pt_BR-faber-medium"
+            case "pt_pt":
+                mdn += "pt_PT-tugão-medium"
+            case "ru_ru":
+                mdn += "ru_RU-irina-medium"
+        return mdn
