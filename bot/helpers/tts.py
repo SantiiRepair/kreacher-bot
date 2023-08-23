88843382af53be757811ce89ed4639d748c31553
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-23 07:59:51 +0000

    Update .gitignore and tts.py

--- bot/helpers/tts.py
+++ bot/helpers/tts.py
@@ -1,11 +1,12 @@
 import os
+import uuid
 import logging
 from bot.helpers.bash import bash
 from bot.helpers.piper import get_model
 
 c = os.path.dirname(os.path.abspath(__file__))
 models = os.path.join(c, "../models")
-
+tmp = os.path.join(c, "../tmp")
 
 async def tts(text: str, output_file: str):
     model = get_model()
@@ -14,9 +15,16 @@ async def tts(text: str, output_file: str):
             os.makedirs(models)
         elif not os.path.exists(os.path.dirname(output_file)):
             os.makedirs(os.path.dirname(output_file))
+        elif not os.path.exists(tmp):
+            os.makedirs(tmp)
+        tmpf = f"{tmp}/{uuid.uuid4()}.txt"
+        with open(tmpf, "w") as temp_text:
+            temp_text.write(text)
+            temp_text.close()
         await bash(
-            f"echo '{text}' | piper -m {model} --download-dir {models} --data-dir {models} -f {output_file}"
+            f"piper -m {model} --download-dir {models} --data-dir {models} -f {output_file} < {tmpf}"
         )
+        os.remove(tmpf)
     except Exception as e:
         logging.error(e)
         raise e
