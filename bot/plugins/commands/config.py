1248cc6f6b368e503f352866622feff006b0f560
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-23 09:02:07 +0000

    Update config.py

--- bot/plugins/commands/config.py
+++ bot/plugins/commands/config.py
@@ -1,10 +1,10 @@
 import os
-from tinydb import TinyDB, Query
+# from tinydb import TinyDB, Query
 from pyrogram.types import Message
 from bot import assistant, kreacher
 from pyrogram import filters, Client
 from bot.decorators.only_admins import only_admins
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+# from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 c = os.path.dirname(os.path.abspath(__file__))
 
@@ -12,10 +12,10 @@ c = os.path.dirname(os.path.abspath(__file__))
 @kreacher.on_message(filters.regex(pattern="^[!?/]config"))
 @only_admins
 async def _(client: Client, message: Message):
-    registry = os.path.join(c, "../dbs/registry.json")
-    db = TinyDB(registry)
-    groups = db.table("groups")
-    user = groups.search(Query().id == message.chat.id)
+    # registry = os.path.join(c, "../dbs/registry.json")
+    # db = TinyDB(registry)
+    # groups = db.table("groups")
+    # user = groups.search(Query().id == message.chat.id)
     common = await assistant.get
     if len(common) > 0:
         print(common)
