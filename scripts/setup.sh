0dc748334291cb377e95ec94bb7cfa3ed0c77e50
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-09-19 21:10:05 +0000

    Update 4 files

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,5 +2,5 @@
 
 pip3 install --no-deps -U pytgcalls==3.0.0.dev24 tgcalls==3.0.0.dev6 && pip3 install -r requirements.txt
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb; sudo apt-get -fy install
-rm -rf google-chrome-stable_current_amd64.deb; sudo apt-get update
+dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install
+rm -rf google-chrome-stable_current_amd64.deb; apt-get update
