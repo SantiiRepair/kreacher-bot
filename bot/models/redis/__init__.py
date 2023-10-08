9c7f1528c060e7d6508b168ce2a2b613a07a8635
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-10-08 19:49:38 +0000

    Update __init__.py and requirements.txt

--- bot/models/redis/__init__.py
+++ bot/models/redis/__init__.py
@@ -1,9 +1,6 @@
 from bot import r
 from typing import Tuple, Union
 
-# ------------------------------------------------------------------------
-
-
 def add_to_queue(
     group_id: str,
     from_user: str,
@@ -42,7 +39,7 @@ def next_in_queue(group_id: str) -> Union[Tuple, None]:
     value = queue[group_id]
     if group_id not in queue:
         return None
-    for i, v in range(len(value)):
+    for i in range(len(value)):
         if value[i].get("is_playing"):
             next = value[i + 1]
             values = (
@@ -62,7 +59,7 @@ def previous_in_queue(group_id: str) -> Union[Tuple, None]:
     value = queue[group_id]
     if group_id not in queue:
         return None
-    for i, v in range(len(value)):
+    for i in range(len(value)):
         if value[i].get("is_playing"):
             next = value[i - 1]
             values = (
@@ -80,32 +77,35 @@ def previous_in_queue(group_id: str) -> Union[Tuple, None]:
 def remove_queue(group_id: str) -> None:
     r.hdel("queues", group_id)
 
-
-def get_last_queue_position(group_id: str) -> Union[int, None]:
+def get_current_position_in_queue(group_id: str) -> Union[int, None]:
     queue = r.hgetall("queues")
     if group_id not in queue:
         return None
-    value = list(queue.values())[-1]
+    value = queue[group_id].values()[-1]
+    for i in range(len(values)):
+        if values[i].get("is_playing"):
     return value["position"]
 
-def update_is_played():
-def next_in_queue(group_id: str) -> Union[Tuple, None]:
-    values = ()
+def get_last_position_in_queue(group_id: str) -> Union[int, None]:
     queue = r.hgetall("queues")
-    value = queue[group_id]
     if group_id not in queue:
         return None
-    for i in range(len(value)):
-        if value[i].get("is_playing"):
-            next = value[i + 1]
-            values = (
-                next["from_user"],
-                next["is_playing"],
-                next["position"],
-                next["date"],
-                next["file"],
-                next["type"],
-            )
-            return values
+    value = queue[group_id].values()[-1]
+    return value["position"]
+
+def update_is_played_in_queue(action: str) -> None:
+    queue = r.hgetall("queues")
+    values = queue[group_id]
+    if group_id not in queue:
+        return None
+    for i in range(len(values)):
+        if values[i].get("is_playing"):
+            if action == "previous":
+                values[i]["is_playing"] = False
+                values[i - 1]["is_playing"] = True
+                return r.hset("queues", group_id, values)
+            elif action == "next":
+                values[i]["is_playing"] = False
+                values[i + 1]["is_playing"] = True
+                return r.hset("queues", group_id, values)
     return None
-# ------------------------------------------------------------------------
