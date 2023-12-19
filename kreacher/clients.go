9af0e0bf0d28a4fbe755520008d8670c5c2a140c
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-12-19 14:30:29 +0000

    refactor: redis params

--- kreacher/clients.go
+++ kreacher/clients.go
@@ -1,6 +1,7 @@
 package kreacher
 
 import (
+	"fmt"
 	"time"
 
 	td "github.com/gotd/td/telegram"
@@ -9,10 +10,10 @@ import (
 )
 
 var rdb = redis.NewClient(&redis.Options{
-	Addr:     "localhost:6379",
-	Password: "", // no password set
-	DB:       0,  // use default DB
-	Protocol: 3,  // specify 2 for RESP 2 or 3 for RESP 3
+	Addr:     fmt.Sprintf("%s:%d", NewConfig().RedisHost, NewConfig().RedisPort),
+	Password: NewConfig().RedisPassword,
+	DB:       0, // use default DB
+	Protocol: 3, // specify 2 for RESP 2 or 3 for RESP 3
 })
 
 var kbot, _ = tele.NewBot(tele.Settings{
