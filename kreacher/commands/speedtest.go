6751d029be8fea9d9579b74c6cd66a87a8475f31
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-12-20 22:40:12 +0000

    removed unused stuff

--- kreacher/commands/speedtest.go
+++ kreacher/commands/speedtest.go
@@ -9,25 +9,30 @@ import (
 
 func Speedtest(c tele.Context) error {
 	err := c.Send("<b><i>Kreacher is here to serve you.\n\nRunning Speedtest...</i></b> 📶", tele.ParseMode(tele.ModeHTML))
-  
+
 	if err != nil {
 		return err
 	}
 
 	st := helpers.Speedtest()
-  c.Delete()
+	if st != nil {
+		c.Delete()
+		caption := fmt.Sprintf("<b>Speedtest Results\n\nClient:</b>\n<b><i>ISP:</i></b> %s\n<b><i>Country:</i></b> %s\n\n<b><i>Server:</i></b>\n<b><i>Name:</i></b> %s\n<b><i>Country:</i></b> %s\n<b><i>Sponsor:</i></b> %s\n<b><i>Latency:</i></b> %s\n<b><i>Ping:</i></b> %s",
+			"",
+			st.Country,
+			st.Name,
+			st.Country,
+			st.Sponsor,
+			st.Latency,
+			st.TestDuration.Ping,
+		)
 
-	caption := fmt.Sprintf("<b>Speedtest Results\n\nClient</b>:\n<b><i>ISP: %s\nCountry: %s\n\nServer:\nName: %s\nCountry: %s\nSponsor: %s\nLatency: %s\nPing: %s</i></b>",
-		"",
-    st.Country,
-    st.Name,
-    st.Country, 
-    st.Sponsor,
-    st.Latency,
-    st.TestDuration.Ping,
-  )
+		err = c.Send(caption, tele.ParseMode(tele.ModeHTML))
 
-  err = c.Send(caption, tele.ParseMode(tele.ModeHTML))
-  
+		return err
+	}
+
+	err = c.Send("<b><i>The speedtest could not be performed</i></b>", tele.ParseMode(tele.ModeHTML))
+
 	return err
 }
