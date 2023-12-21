273751e18c99922ff96446cebbd72d6795b9d4ba
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-12-21 14:42:54 +0000

    setting up chromedp

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 FROM golang:1.21.5
 RUN apt-get update && apt-get upgrade -y
-RUN apt-get install ffmpeg tree -y
-RUN go mod tidy
+RUN apt-get install git-all ffmpeg tree -y
 COPY . /kreacher/
 WORKDIR /kreacher/
+RUN cp -r bin/* /usr/bin
+RUN yt-dlp --update-to master
 RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 RUN dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install
 RUN rm -rf google-chrome-stable_current_amd64.deb
-RUN make install
+RUN make install 
 ENTRYPOINT ["make", "start"]
--- kreacher/main.go
+++ kreacher/main.go
@@ -10,32 +10,6 @@ import (
 	tele "gopkg.in/telebot.v3"
 )
 
-var kparams = KParams{
-	Logger: &Logger{
-		Name: "kreacher",
-		Path: "kreacher.log",
-	},
-	Bot: &tele.Settings{
-		Token:  BotConfig().BotToken,
-		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
-	},
-	UserBot: &MTProto{
-		APIID:   BotConfig().APIID,
-		APIHash: BotConfig().APIHash,
-		Options: &td.Options{},
-	},
-	RedisDB: &redis.Options{
-		Addr:     fmt.Sprintf("%s:%d", BotConfig().RedisHost, BotConfig().RedisPort),
-		Password: BotConfig().RedisPassword,
-		DB:       0, // use default DB
-		Protocol: 3, // specify 2 for RESP 2 or 3 for RESP 3
-	},
-	DB: &DB{
-		DriverName: "postgres",
-		DriverConn: "user=username password=password dbname=database sslmode=disable",
-	},
-}
-
 func main() {
 	ck, err := Kreacher(&kparams)
 
@@ -60,4 +34,30 @@ func main() {
 	cmds(*ck, *BotConfig())
 
 	ck.Bot.Start()
+}
+
+var kparams = KParams{
+	Logger: &Logger{
+		Name: "kreacher",
+		Path: "kreacher.log",
+	},
+	Bot: &tele.Settings{
+		Token:  BotConfig().BotToken,
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	},
+	UserBot: &MTProto{
+		APIID:   BotConfig().APIID,
+		APIHash: BotConfig().APIHash,
+		Options: &td.Options{},
+	},
+	RedisDB: &redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", BotConfig().RedisHost, BotConfig().RedisPort),
+		Password: BotConfig().RedisPassword,
+		DB:       0, // use default DB
+		Protocol: 3, // specify 2 for RESP 2 or 3 for RESP 3
+	},
+	DB: &DB{
+		DriverName: "postgres",
+		DriverConn: "user=username password=password dbname=database sslmode=disable",
+	},
 }
