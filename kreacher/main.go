94f632b47183846654840fbbbddaff88b59a7777
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-12-22 14:08:13 +0000

    removed command, moved redis dir

--- kreacher/main.go
+++ kreacher/main.go
@@ -70,7 +70,6 @@ func main() {
 	if err := bot.SetCommands([]tele.Command{
 		{Text: "config", Description: "Set the bot's configuration"},
 		{Text: "help", Description: "How to use this"},
-		{Text: "leave", Description: "Leave the voice chat"},
 		{Text: "ping", Description: "Check the server's latency"},
 		{Text: "play_book", Description: "Play a pdf or epub file as an audio book"},
 		{Text: "play_song", Description: "Play audio in the voice chat"},
