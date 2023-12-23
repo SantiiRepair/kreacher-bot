85b479284e0c1463cdaad8044c416d611a921c24
Santiago Ramirez <santiirepair@gmail.com>
2023-12-23 03:12:06 -0400

    fix: golang wrong download link in sh file

--- setenv.sh
+++ setenv.sh
@@ -27,10 +27,10 @@ if [ "$1" == "--local" ]; then
     fi
 
     if [ -z $GOPATH ]; then
-        wget https://go.dev/dl/go1.21.5.linux-arm64.tar.gz
-        sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.21.5.linux-arm64.tar.gz
+        wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz
+        sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz
         echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
-        rm -rf go1.21.5.linux-arm64.tar.gz
+        rm -rf go1.21.5.linux-amd64.tar.gz
     fi
 
     if [[ -z $YT_DLP && -z $SPEEDTEST ]]; then
