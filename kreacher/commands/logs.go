30dcb4826d6219d6cf6285de76db3621ab1af736
Santiago Ramirez <miguelsantiago1940@gmail.com>
2024-06-28 02:01:45 +0000

    updates

--- kreacher/commands/logs.go
+++ kreacher/commands/logs.go
@@ -1,12 +1,15 @@
 package commands
 
 import (
+	"path"
+
 	tele "gopkg.in/telebot.v3"
 	"santiirepair.dev/kreacher/logger"
 )
 
 func logs(c tele.Context) error {
-	file := &tele.Document{File: tele.FromDisk(logger.LogsPath)}
-
-	return c.Send(file)
+	return c.Send(&tele.Document{
+		File:     tele.FromDisk(logger.LogsPath),
+		FileName: path.Base(logger.LogsPath),
+	})
 }
