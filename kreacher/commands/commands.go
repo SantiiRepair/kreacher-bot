ebc4f2a175716f3e85ae2a330f6a5a270d988fe5
Santiago Ramirez <miguelsantiago1940@gmail.com>
2024-09-14 05:26:36 +0000

    removed shitty helpers

--- kreacher/commands/commands.go
+++ kreacher/commands/commands.go
@@ -2,14 +2,10 @@ package commands
 
 import (
 	"context"
-	"fmt"
 	"log"
 
-	"go.uber.org/zap"
-	tele "gopkg.in/telebot.v3"
 	"gopkg.in/telebot.v3/middleware"
 	"santiirepair.dev/kreacher/core"
-	"santiirepair.dev/kreacher/logger"
 )
 
 const (
@@ -35,51 +31,16 @@ func Start() {
 
 	onlyAdmin := middleware.Whitelist([]int64{mantainer.ID}...)
 
-	core.B.Handle(CONFIG, func(c tele.Context) error {
-		sent := c.Send("Hello to you too!")
-		if sent != nil {
-			logger.Error("config command", zap.Error(sent))
-		}
+	core.B.Handle(CONFIG, config)
 
-		return nil
-	})
-
-	core.B.Handle(HELP, func(c tele.Context) error {
-		if sent := c.Send(
-			"For info on how to use the bot we recommend reading our [wiki](https://github.com/SantiiRepair/kreacher-bot/wiki)",
-			tele.ParseMode(tele.ModeMarkdownV2),
-			tele.NoPreview,
-		); sent != nil {
-			logger.Error("help command", zap.Error(sent))
-		}
-
-		return nil
-	})
-
+	core.B.Handle(HELP, help)
 	core.B.Handle(LEAVE, leave)
 	core.B.Handle(ACCIO, accio)
 
-	core.B.Handle(PLAY_BOOK, func(c tele.Context) error {
-		sent := c.Send("Hello to you too!")
-		if sent != nil {
-			logger.Error("play book command", zap.Error(sent))
-		}
-
-		return nil
-	})
-
+	core.B.Handle(PLAY_BOOK, bplay)
 	core.B.Handle(PLAY_AUDIO, play)
 	core.B.Handle(PLAY_VIDEO, vplay)
-
-	core.B.Handle(STREAMING, func(c tele.Context) error {
-		fmt.Println("Got a hello message")
-		sent := c.Send("Hello to you too!")
-		if sent != nil {
-			logger.Error("streaming command", zap.Error(sent))
-		}
-
-		return nil
-	})
+	core.B.Handle(STREAMING, streaming)
 
 	core.B.Handle(SH, sh, onlyAdmin)
 	core.B.Handle(LOGS, logs, onlyAdmin)
--- kreacher/commands/help.go
+++ kreacher/commands/help.go
@@ -4,5 +4,9 @@ import tele "gopkg.in/telebot.v3"
 
 func help(c tele.Context) error {
 
-	return nil
+	return c.Send(
+		"For info on how to use the bot we recommend reading our [wiki](https://github.com/SantiiRepair/kreacher-bot/wiki)",
+		tele.ParseMode(tele.ModeMarkdownV2),
+		tele.NoPreview,
+	)
 }
